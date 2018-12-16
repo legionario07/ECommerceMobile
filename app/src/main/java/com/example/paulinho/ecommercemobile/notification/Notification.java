e10c360f2a8d2223382cb258ab361d2ada679f11
legionario07 <paulinho.legionario@hotmail.com>
2018-12-16 13:29:26 -0200

    Corrigido a Notificação para se exibido

--- app/src/main/java/com/example/paulinho/ecommercemobile/notification/Notification.java
+++ app/src/main/java/com/example/paulinho/ecommercemobile/notification/Notification.java
@@ -1,9 +1,11 @@
 package com.example.paulinho.ecommercemobile.notification;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 
@@ -16,11 +18,13 @@ import static android.content.Context.VIBRATOR_SERVICE;
 
 public class Notification {
 
+    private static NotificationManager mNotificationManager;
+
     public static void criarNotificacao(Context context, String mensagem, Produto produto) {
 
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+        NotificationCompat.Builder builder =  new NotificationCompat.Builder(context, "notify_001")
                 .setTicker(ConstraintUtils.ECOMMERCE_MOBILE)
                 .setContentTitle(produto.getTitle())
                 .setContentText(mensagem)
@@ -34,6 +38,18 @@ public class Notification {
             builder.setSmallIcon(R.mipmap.ic_launcher);
         }
 
+        mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "YOUR_CHANNEL_ID";
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            mNotificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);
+        }
+
 
         android.app.Notification not = builder.build();
         not.flags = android.app.Notification.FLAG_AUTO_CANCEL;
@@ -42,8 +58,7 @@ public class Notification {
         long milliseconds = 500;
         vibrator.vibrate(milliseconds);
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, not);
+        mNotificationManager.notify(1, not);
 
     }
 
